9139a9874c740eafcc3b50a00eaba906de23dca7
Juan Carlos Felizzola <jfelizzola@gasolutions.com.co>
2019-12-20 18:15:31 -0500

    Archivos hasta sección sistema de grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Button, Card, CardMedia, makeStyles, CardContent } from '@material-ui/core';
+import { indigo, red, lightBlue, amber } from '@material-ui/core/colors';
+
+import Title from './components/Title';
+
+
 import './App.css';
 
+const indigo400 = indigo[400];
+const red400 = red[400];
+const lightBlue400 = lightBlue[400];
+const amber400 = amber[400];
+
+const useStyles = makeStyles(theme => ({
+  containerCards: {
+    backgroundColor: indigo400,
+    padding: '50px',
+    display: 'grid',
+    gridTemplatesColumns: 'repeat(3, 1fr)',
+    gridGap: '1em'
+  },
+  card: {
+    display: 'flex'
+  },
+  backgroundHeart: {
+    backgroundColor: red400,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  backgroundWifi: {
+    backgroundColor: lightBlue400,
+    display: 'flex',
+    alignItems: 'center'
+  },
+  backgroundFavorites: {
+    backgroundColor: amber400,
+    display: 'flex',
+    alignItems: 'center'
+  }
+}));
+
 function App() {
+  const classes = useStyles();
+  classes.backgroundHeart += " card-media"
+  classes.backgroundWifi += " card-media"
+  classes.backgroundFavorites += " card-media"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className="Header-background">
+        <div style={{ "width": "80%", "margin": "0 auto" }}>
+
+          <div className="Header-main">
+            <Title/>
+            <Button variant="contained" color="primary">Crear cuenta gratuita</Button>
+            <img className="Header-ilustration" src={ process.env.PUBLIC_URL + '/images/top-background.png' } alt="Background places"/>
+          </div>
+
+        </div>
+      </div>
+
+      <div className="container-cards">
+        <Card className="card">
+          <CardMedia className="card-media heart">
+            <i className="far fa-heart icon"></i>
+          </CardMedia>
+          <CardContent>
+            <div>
+              <h3>Calificaciones con emociones</h3>
+              <p>Califica tus lugares con experiencias, no con números</p>
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card className="card">
+          <CardMedia className="card-media wifi">
+            <i className="fas fa-wifi icon"></i>
+          </CardMedia>
+          <CardContent>
+            <div>
+              <h3>¿Sin internet? Sin problemas</h3>
+              <p>Places funciona sin internet o en conexiones lentas</p>
+            </div>
+          </CardContent>
+        </Card>
+
+        <Card className="card">
+          <CardMedia className="card-media favorite">
+            <i className="far fa-star icon"></i>
+          </CardMedia>
+          <CardContent>
+            <div>
+              <h3>Tus lugares favoritos</h3>
+              <p>Define tu lista de sitios favoritos</p>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </section>
   );
 }
 
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Title(props) {
+    return(
+        <div>
+            <h1>Places</h1>
+            <p>Descubre lugares de manera simple</p>
+        </div>
+    )
+}
